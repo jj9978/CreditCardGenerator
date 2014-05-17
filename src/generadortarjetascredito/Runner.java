c89c9ce025d0d184d671869f9dabce77d5c3da98
Andrés <insane-stdios@hotmail.com>
2014-05-17 12:33:50 -0500

    cvv update

--- src/generadortarjetascredito/Runner.java
+++ src/generadortarjetascredito/Runner.java
@@ -1,6 +1,7 @@
 package generadortarjetascredito;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
@@ -15,28 +16,31 @@ public class Runner {
         Scanner in = new Scanner(System.in);
         int [] ccn = new int[10];
         int [] n = new int[10];
+        System.out.println("Escribe 10 números entre 0 y 9.");
         for(int f=0;f<ccn.length;f++){
-            System.out.println("Escribe 10 números entre 0 y 9.");
-            ccn[f] =in.nextInt(); 
+            System.out.print(">>");
+            ccn[f] =in.nextInt();
         }
-        for(int i=0;i<n.length;i++){
-        System.out.println("Este es el número ingresado"+i);
+        for(int i=0;i<n.length;i++)
             if(i%2!=0)
                n[i]+=n[i] = i*2;
+            else{
+               n[i] = i;
             System.out.println(n[i]);
-        }
+            }
+        System.out.println("Este es el "+ Arrays.toString(n));
 }
     //Generamos el valor del CVV.
     static void cvv(){
         System.out.println("cvv");
+        System.out.print(">>");
         int [] cvvarray = new int[3];
         Random randomcvv = new Random();
-        for (int cvv = 0; cvv <3; cvv++){
+        for(int j=0;j<cvvarray.length;j++){
             int randomInt = randomcvv.nextInt(10);
-            for(int j=0;j<cvvarray.length;j++){
-                cvvarray[j] = randomInt;
-                System.out.println(">> " + cvvarray[j]);
-            }
+            int a = cvvarray[j] = randomInt;
+            System.out.print(a);
         }
+        System.out.println("");
     }
 }
